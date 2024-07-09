986a4ad3abb369ab50c2ed9c476bcb217dad5990
joaovpimenta <joaovpiment@gmail.com>
2024-07-09 09:56:14 -0300

    Commit inicial
    - Projeto: busca-cnpj
    - Group ID: com.joaopimenta
    - Artifact ID: busca-cnpj
    - Versão: 0.0.1-SNAPSHOT
    - Descrição: Um projeto para fazer consultas a detalhes de um CNPJ
    - Objetivo: Criar uma CLI para buscar dados na web e aplicar filtros
    - Tecnologias: Spring Framework, Spring Shell, Spring Web, GraalVM
    - Configuração inicial incluindo estrutura do projeto e arquivos de configuração.

---
+++ src/main/java/com/joaopimenta/busca_cnpj/BuscaCnpjApplication.java
@@ -0,0 +1,13 @@
+package com.joaopimenta.busca_cnpj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuscaCnpjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuscaCnpjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joaopimenta/busca_cnpj/BuscaCnpjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaopimenta.busca_cnpj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuscaCnpjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
